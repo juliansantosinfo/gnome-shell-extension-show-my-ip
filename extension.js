dc1b7d03c0da6430fdc351595c7cf0f1e85412de
Julian de Almeida Santos <julian.santos.info@gmail.com>
2026-03-14 22:40:05 -0300

    feat: Migrate public IP fetching to ifconfig.me and refactor local IP retrieval using Gio.Subprocess.

--- extension.js
+++ extension.js
@@ -17,6 +17,7 @@
  */
 
 import St from 'gi://St';
+import Gio from 'gi://Gio';
 import Clutter from 'gi://Clutter';
 import GLib from 'gi://GLib';
 import Soup from 'gi://Soup';
@@ -24,14 +25,13 @@ import { panel } from 'resource:///org/gnome/shell/ui/main.js';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import { Button } from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
-const ByteArray = imports.byteArray;
-const IP_SERVICE_URL = 'https://api.ipify.org'
+const IP_SERVICE_URL = 'https://ifconfig.me/ip';
 
 export default class ShowMyIpExtension extends Extension {
     enable() {
-        const box = new St.BoxLayout({ vertical: false });
+        this._box = new St.BoxLayout({ vertical: false });
         const buttonStyle = 0;
-        const buttonInternalName = "Show My IP";
+        const buttonInternalName = 'Show My IP';
         const buttonIsInteractive = false;
 
         const lanIcon = new St.Icon({
@@ -45,28 +45,28 @@ export default class ShowMyIpExtension extends Extension {
         });
 
         this._lanLabel = new St.Label({
-            text: "Loading...",
+            text: 'Loading...',
             y_align: Clutter.ActorAlign.CENTER,
         });
         this._wanLabel = new St.Label({
-            text: "Loading...",
+            text: 'Loading...',
             y_align: Clutter.ActorAlign.CENTER,
         });
 
         this._indicator = new Button(
-            buttonStyle, 
-            buttonInternalName, 
+            buttonStyle,
+            buttonInternalName,
             buttonIsInteractive
         );
 
-        box.add_child(lanIcon);
-        box.add_child(this._lanLabel);
-        box.add_child(wanIcon);
-        box.add_child(this._wanLabel);
+        this._box.add_child(lanIcon);
+        this._box.add_child(this._lanLabel);
+        this._box.add_child(wanIcon);
+        this._box.add_child(this._wanLabel);
 
-        this._indicator.add_child(box);
+        this._indicator.add_child(this._box);
 
-        panel.addToStatusArea('show-my-ip', this._indicator, 0);
+        panel.addToStatusArea('show-my-ip', this._indicator, 1, 'left');
 
         this._httpSession = new Soup.Session();
 
@@ -87,47 +87,77 @@ export default class ShowMyIpExtension extends Extension {
             this._httpSession.abort();
             this._httpSession = null;
         }
-        if (this._indicator) {
-            this._indicator.destroy();
-        }
+        this._indicator?.destroy();
+        this._indicator = null;
+        this._box = null;
+        this._lanLabel = null;
+        this._wanLabel = null;
     }
 
     _updateIp() {
-        const ipLocal = this._getLocalIp();
-        this._lanLabel.set_text(ipLocal);
-        this._getPublicIp().then(ipPublic => {
-            this._wanLabel.set_text(ipPublic);
-        }).catch(() => {
-            this._wanLabel.set_text('IP error');
-        });
+        this._getLocalIp()
+            .then(ipLocal => {
+                if (this._lanLabel)
+                    this._lanLabel.set_text(ipLocal);
+            })
+            .catch(err => {
+                console.error(`Show My IP: LAN Error ${err}`);
+                if (this._lanLabel)
+                    this._lanLabel.set_text('LAN error');
+            });
+
+        this._getPublicIp()
+            .then(ipPublic => {
+                if (this._wanLabel)
+                    this._wanLabel.set_text(ipPublic);
+            })
+            .catch(err => {
+                console.error(`Show My IP: WAN Error ${err}`);
+                if (this._wanLabel)
+                    this._wanLabel.set_text('WAN error');
+            });
     }
 
     _getLocalIp() {
-        try {
-            let command = "bash -c \"ip route get 1.1.1.1 | awk '{for(i=1;i<=NF;i++) if ($i==\\\"src\\\") print $(i+1)}'\"";
-
-            let [ok, stdout, stderr, status] = GLib.spawn_command_line_sync(command);
-
-            if (ok && status === 0) {
-                let ip = ByteArray.toString(stdout).trim();
-                return ip || "IP not found";
-            } else {
-                return "IP not found";
+        return new Promise((resolve, reject) => {
+            try {
+                const proc = Gio.Subprocess.new(
+                    ['bash', '-c', "ip route get 1.1.1.1 | awk '{for(i=1;i<=NF;i++) if ($i==\"src\") print $(i+1)}'"],
+                    Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+                );
+                proc.communicate_utf8_async(null, null, (p, res) => {
+                    try {
+                        const [, stdout, stderr] = p.communicate_utf8_finish(res);
+                        if (p.get_successful()) {
+                            resolve(stdout.trim() || 'IP not found');
+                        } else {
+                            console.error(`Show My IP: Subprocess error ${stderr}`);
+                            resolve('IP not found');
+                        }
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            } catch (e) {
+                reject(e);
             }
-        } catch (e) {
-            return "IP error";
-        }
+        });
     }
 
     _getPublicIp() {
         return new Promise((resolve, reject) => {
+            if (!this._httpSession) {
+                reject(new Error('No HTTP session'));
+                return;
+            }
+
             const message = Soup.Message.new('GET', IP_SERVICE_URL);
 
             this._httpSession.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null, (session, result) => {
                 try {
                     const bytes = session.send_and_read_finish(result);
                     if (message.get_status() === Soup.Status.OK) {
-                        const response = ByteArray.toString(bytes.get_data());
+                        const response = new TextDecoder().decode(bytes.toArray());
                         resolve(response.trim());
                     } else {
                         reject(new Error(`HTTP error: ${message.get_status()}`));
@@ -137,24 +167,5 @@ export default class ShowMyIpExtension extends Extension {
                 }
             });
         });
-    }
-
-    _getPublicIpByCurl() {
-        try {
-            let command = `/bin/sh -c "curl -s ifconfig.me"`;
-
-            let [ok, stdout, stderr, status] = GLib.spawn_command_line_sync(command);
-
-            if (ok && status === 0) {
-                let ip = ByteArray.toString(stdout).trim();
-                return ip;
-            } else {
-                let ip = "IP not found"
-                return ip;
-            }
-        } catch (e) {
-            ip = "IP error";
-            return ip;
-        }
     }
 }
